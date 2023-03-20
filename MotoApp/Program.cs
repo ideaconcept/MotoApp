da81cfb11cf43d6c5ca2b0eb281a1891f7eabd4b
Jacek Hawrylczak <jacek.hawrylczak@ideaconcept.expert>
2023-03-20 23:21:33 +0100

    Pierwszy commit

---
+++ MotoApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using MotoApp;
+
+var stack = new BasicStack();
+stack.Push(67.7);
+stack.Push(11);
+stack.Push(36);
+stack.Push(1);
+
+double sum = 0.0;
+
+while (stack.Count > 0)
+{
+    double item = stack.Pop();
+    Console.WriteLine($"Item: {item}");
+    sum += item;
+}
+
+Console.WriteLine($"Sum: {sum}");
+
+var licznik = stack.Count;
+Console.WriteLine($"Count: {licznik}");
